3152501307cda946c05c15414638d85da3f7da7b
Chaitanya Gangavarapu <chaitanya.gangavarapu@gmail.com>
2019-01-22 15:44:23 -0800

    finished first two sets

--- SimpleDomainModel/SimpleDomainModel/main.swift
+++ SimpleDomainModel/SimpleDomainModel/main.swift
@@ -28,16 +28,60 @@ public struct Money {
   public var currency : String
   
   public func convert(_ to: String) -> Money {
+    var newAmt: Int = amount;
+    if currency == "USD" {
+        if to == "GBP" {
+            newAmt = amount/2;
+        } else if to == "CAN" {
+            newAmt = Int(Double(amount) * 1.25);
+        } else if to == "EUR" {
+            newAmt = Int(Double(amount) * 1.5);
+        }
+        
+    } else if currency == "GBP" {
+        if to == "USD" {
+            newAmt = amount * 2
+        } else if to == "CAN" {
+            return self.convert("USD").convert("CAN");
+        } else if to == "EUR" {
+            return self.convert("USD").convert("EUR");
+        }
+        
+    } else if currency == "CAN" {
+        if to == "USD" {
+            newAmt = Int(Double(amount) / 1.25);
+        } else if to == "GBP" {
+            return self.convert("GBP");
+        } else if to == "EUR" {
+            return self.convert("USD").convert("CAN");
+        }
+        
+    } else if currency == "EUR" {
+        if to == "USD" {
+            newAmt = Int(Double(amount) / 1.5);
+        } else if to == "CAN" {
+            return self.convert("USD").convert("CAN");
+        } else if to == "GBP" {
+            return self.convert("USD").convert("GBP");
+        }
+        
+    }
+    return Money(amount: newAmt, currency: to);
   }
   
   public func add(_ to: Money) -> Money {
+    let newAmt: Int = self.convert(to.currency).amount
+    return Money(amount: to.amount + newAmt, currency: to.currency)
   }
+    
   public func subtract(_ from: Money) -> Money {
+    let newAmt: Int = self.convert(from.currency).amount
+    return Money(amount: newAmt - from.amount, currency: from.currency)
   }
 }
 
-////////////////////////////////////
-// Job
+//////////////////////////////////////
+//// Job
 //
 open class Job {
   fileprivate var title : String
@@ -47,66 +91,82 @@ open class Job {
     case Hourly(Double)
     case Salary(Int)
   }
-  
+
   public init(title : String, type : JobType) {
+    self.title = title;
+    self.type = type;
   }
-  
+
   open func calculateIncome(_ hours: Int) -> Int {
+    switch type {
+    case .Hourly (let money):
+        return Int(money * Double(hours));
+    case .Salary (let money):
+        return money;
+    }
   }
-  
+
   open func raise(_ amt : Double) {
+    switch type {
+    case .Hourly (let money):
+        type = .Hourly(money + amt);
+    case .Salary (let money):
+        type = .Salary(money + Int(amt));
+
   }
 }
+}
 
-////////////////////////////////////
-// Person
-//
+//////////////////////////////////////
+//// Person
+////
 open class Person {
   open var firstName : String = ""
   open var lastName : String = ""
   open var age : Int = 0
 
   fileprivate var _job : Job? = nil
+    
   open var job : Job? {
     get { }
     set(value) {
     }
   }
-  
+
   fileprivate var _spouse : Person? = nil
   open var spouse : Person? {
     get { }
     set(value) {
     }
   }
-  
+
   public init(firstName : String, lastName: String, age : Int) {
     self.firstName = firstName
     self.lastName = lastName
     self.age = age
   }
-  
+
   open func toString() -> String {
   }
 }
 
-////////////////////////////////////
-// Family
+//////////////////////////////////////
+//// Family
+////
+//open class Family {
+//  fileprivate var members : [Person] = []
 //
-open class Family {
-  fileprivate var members : [Person] = []
-  
-  public init(spouse1: Person, spouse2: Person) {
-  }
-  
-  open func haveChild(_ child: Person) -> Bool {
-  }
-  
-  open func householdIncome() -> Int {
-  }
-}
-
-
-
-
-
+//  public init(spouse1: Person, spouse2: Person) {
+//  }
+//
+//  open func haveChild(_ child: Person) -> Bool {
+//  }
+//
+//  open func householdIncome() -> Int {
+//  }
+//}
+//
+//
+//
+//
+//
--- SimpleDomainModel/SimpleDomainModelTests/JobTests.swift
+++ SimpleDomainModel/SimpleDomainModelTests/JobTests.swift
@@ -5,13 +5,13 @@
 //  Created by Ted Neward on 4/6/16.
 //  Copyright © 2016 Ted Neward. All rights reserved.
 //
-
+//
 import XCTest
 
 import SimpleDomainModel
 
 class JobTests: XCTestCase {
-  
+
   func testCreateSalaryJob() {
     let job = Job(title: "Guest Lecturer", type: Job.JobType.Salary(1000))
     XCTAssert(job.calculateIncome(50) == 1000)
@@ -21,21 +21,21 @@ class JobTests: XCTestCase {
     let job = Job(title: "Janitor", type: Job.JobType.Hourly(15.0))
     XCTAssert(job.calculateIncome(10) == 150)
   }
-  
+
   func testSalariedRaise() {
     let job = Job(title: "Guest Lecturer", type: Job.JobType.Salary(1000))
     XCTAssert(job.calculateIncome(50) == 1000)
-    
+
     job.raise(1000)
     XCTAssert(job.calculateIncome(50) == 2000)
   }
-  
+
   func testHourlyRaise() {
     let job = Job(title: "Janitor", type: Job.JobType.Hourly(15.0))
     XCTAssert(job.calculateIncome(10) == 150)
-    
+
     job.raise(1.0)
     XCTAssert(job.calculateIncome(10) == 160)
   }
-  
+
 }
